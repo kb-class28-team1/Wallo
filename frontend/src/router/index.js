c540e25679f1ce980e7b5f2f9a32356db9e99f96
jinseo <acgrp103@gmail.com>
2026-07-28 17:27:04 +0900

    feat(fe): 공통 네비게이션 라우트 경로 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,7 +11,56 @@ const router = createRouter({
       children: [
         {
           path: '',
+          redirect: { name: 'dashboard' },
+        },
+        {
+          path: 'api/home',
           name: 'dashboard',
+          component: DashboardView,
+        },
+        {
+          path: 'api/institutions',
+          name: 'institutions',
+          component: DashboardView,
+        },
+        {
+          path: 'api/dashboard',
+          name: 'ai-consulting',
+          component: DashboardView,
+        },
+        {
+          path: 'api/challenges/current',
+          name: 'current-challenge',
+          component: DashboardView,
+        },
+        {
+          path: 'api/challenges/:challengeId/feeds',
+          name: 'challenge-feed',
+          component: DashboardView,
+        },
+        {
+          path: 'api/challenges/rankings/weekly',
+          name: 'weekly-ranking',
+          component: DashboardView,
+        },
+        {
+          path: 'api/users/me/challenge-dashboard',
+          name: 'my-challenge',
+          component: DashboardView,
+        },
+        {
+          path: 'api/point-shop',
+          name: 'point-shop',
+          component: DashboardView,
+        },
+        {
+          path: 'api/reports',
+          name: 'reports',
+          component: DashboardView,
+        },
+        {
+          path: 'api/users/profile',
+          name: 'user-profile',
           component: DashboardView,
         },
       ],
