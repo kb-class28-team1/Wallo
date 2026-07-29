585de57225e9a2a4073ab0e18f5073cf2cfcfde8
jinseo <acgrp103@naver.com>
2026-07-29 16:11:28 +0900

    feat: 누락된 더미 고용 페이지 구현

--- frontend/src/views/dashboard/DashboardView.vue
+++ frontend/src/views/dashboard/DashboardView.vue
@@ -1,3 +1,9 @@
 <template>
-  <div></div>
+  <section class="container-fluid p-4">
+    <div class="card border-0 shadow-sm">
+      <div class="card-body p-4">
+        <h1 class="mb-0 fw-bold">대시보드 페이지</h1>
+      </div>
+    </div>
+  </section>
 </template>
