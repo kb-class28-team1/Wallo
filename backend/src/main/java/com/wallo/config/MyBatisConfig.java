66ecf46b766e867395f2051e36552c52da026898
Kim Hyunji <jikim567@naver.com>
2026-07-30 13:47:47 +0900

    chore: 로컬 DB 설정 정리

--- backend/src/main/java/com/wallo/config/MyBatisConfig.java
+++ backend/src/main/java/com/wallo/config/MyBatisConfig.java
@@ -23,7 +23,7 @@ public class MyBatisConfig {
             @Value("${db.driverClassName}") String driverClassName,
             @Value("${db.url}") String url,
             @Value("${db.username}") String username,
-            @Value("1910") String password
+            @Value("${db.password}") String password
     ) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driverClassName);
