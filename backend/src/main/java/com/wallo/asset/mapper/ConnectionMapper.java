40d50f37920e3743ef2c958b5daa0df0bdf73620
KHJ <jin3574@dgu.ac.kr>
2026-07-30 14:26:40 +0900

    feat: 대시보드용 DB 초기화 스크립트 작성, 연동기관 저장 mapper 수정

--- backend/src/main/java/com/wallo/asset/mapper/ConnectionMapper.java
+++ backend/src/main/java/com/wallo/asset/mapper/ConnectionMapper.java
@@ -8,6 +8,7 @@ public interface ConnectionMapper {
 
     int insertConnections(
             @Param("connections") List<ConnectionDto.Result> results,
+            @Param("userId") long userId,
             @Param("loginType") String loginType,
             @Param("loginId") String loginId,
             @Param("loginPassword") String loginPassword
