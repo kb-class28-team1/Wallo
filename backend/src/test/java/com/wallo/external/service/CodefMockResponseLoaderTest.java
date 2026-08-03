41fa1736b8f2a34a59d2c68b962274a5c905fd00
KHJ <jin3574@dgu.ac.kr>
2026-08-03 15:55:17 +0900

    fix: Mock JSON 수정

--- backend/src/test/java/com/wallo/external/service/CodefMockResponseLoaderTest.java
+++ backend/src/test/java/com/wallo/external/service/CodefMockResponseLoaderTest.java
@@ -2,6 +2,7 @@ package com.wallo.external.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wallo.external.dto.CodefDto;
@@ -19,7 +20,7 @@ public class CodefMockResponseLoaderTest {
         CodefDto.AssetData data = new ObjectMapper().convertValue(response.getData(), CodefDto.AssetData.class);
         assertEquals(2, data.getAccounts().size());
         assertEquals(1, data.getLoans().size());
-        assertEquals(3, data.getTransactions().size());
+        assertEquals(1, data.getTransactions().size());
     }
 
     @Test
@@ -29,7 +30,7 @@ public class CodefMockResponseLoaderTest {
         assertSuccessResponse(response);
         CodefDto.AssetData data = new ObjectMapper().convertValue(response.getData(), CodefDto.AssetData.class);
         assertEquals(2, data.getCards().size());
-        assertEquals(4, data.getTransactions().size());
+        assertNull(data.getTransactions());
     }
 
     @Test
