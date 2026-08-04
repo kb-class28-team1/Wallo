b5195df57102f3042c8b3b0998dfe927e6ed65fb
KHJ <jin3574@dgu.ac.kr>
2026-08-04 09:02:27 +0900

    feat: InstitutionController를 ConnectionController로 통합

--- backend/src/main/java/com/wallo/asset/controller/ConnectionController.java
+++ backend/src/main/java/com/wallo/asset/controller/ConnectionController.java
@@ -1,9 +1,10 @@
 package com.wallo.asset.controller;
 
 import com.wallo.auth.CurrentUserProvider;
-import com.wallo.common.response.CommonResponse;
 import com.wallo.asset.dto.ConnectionDto;
 import com.wallo.asset.service.ConnectionService;
+import com.wallo.asset.service.InstitutionService;
+import com.wallo.common.response.CommonResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,20 +12,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/connections")
+@RequestMapping("/api")
 public class ConnectionController {
 
     private final ConnectionService connectionService;
+    private final InstitutionService institutionService;
     private final CurrentUserProvider currentUserProvider;
 
     public ConnectionController(
             ConnectionService connectionService,
-            CurrentUserProvider currentUserProvider) {
+            InstitutionService institutionService,
+            CurrentUserProvider currentUserProvider
+    ) {
         this.connectionService = connectionService;
+        this.institutionService = institutionService;
         this.currentUserProvider = currentUserProvider;
     }
 
-    @PostMapping
+    @GetMapping("/institutions")
+    public CommonResponse<ConnectionDto.InstitutionResponse> getInstitutions() {
+        return CommonResponse.success(institutionService.getInstitutions());
+    }
+
+    @PostMapping("/connections")
     public CommonResponse<ConnectionDto.Response> connectAllAssets(
             @RequestBody ConnectionDto.Request request
     ) {
