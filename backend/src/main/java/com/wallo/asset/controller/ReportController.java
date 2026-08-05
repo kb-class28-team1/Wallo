b06a4738738e36fe80623d602e8234806cadb8cc
jinseo <acgrp103@naver.com>
2026-08-05 10:05:21 +0900

    fix: ReportController 빈 이름 충돌 해결

--- backend/src/main/java/com/wallo/asset/controller/ReportController.java
+++ backend/src/main/java/com/wallo/asset/controller/ReportController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("assetReportController")
 @RequestMapping("/api/reports")
 public class ReportController {
 
